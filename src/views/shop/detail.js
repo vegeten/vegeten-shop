3338e9eaabc1197c565e4940a6b1fc03edc949ea
jiyoonZ <juneky416@gmail.com>
2022-05-26 00:50:29 +0900

    feat:상품 상세보기

---
+++ src/views/shop/detail.js
@@ -0,0 +1,21 @@
+import renderFooter from '../components/footer.js';
+import renderNav from '../components/nav.js';
+
+const mockUserAPI = {
+  email: 'test@test.com',
+  fullName: '김정현',
+  password: '12341234',
+  phoneNumber: '010-5628-9304',
+  address: {
+    postalCode: '1234',
+    address1: '마포구 합정동',
+    address2: '123-123',
+  },
+  role: 'basic-user',
+};
+
+renderNav(mockUserAPI.role === 'basic-user' ? false : true);
+renderFooter();
+
+const selectedOption = document.querySelector('select#total-count option:checked').value;
+console.log(selectedOption);
