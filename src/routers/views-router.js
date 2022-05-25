6d91744d3ef338a9f07b77a0c8c57bd9a680511e
task11 <task11@naver.com>
2022-05-25 15:58:38 +0900

    style: Mypage 페이지 레이아웃 마크업 & 스타일링

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -9,6 +9,7 @@ const viewsRouter = express.Router();
 viewsRouter.use('/', serveStatic('home'));
 viewsRouter.use('/register', serveStatic('register'));
 viewsRouter.use('/login', serveStatic('login'));
+viewsRouter.use('/mypage', serveStatic('mypage'));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
---
+++ src/views/mypage/mypage.js
@@ -0,0 +1,6 @@
+import renderNav from '../components/nav.js';
+import renderFooter from '../components/footer.js';
+
+
+renderNav();
+renderFooter();
