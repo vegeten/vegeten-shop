3d2d705a404640e5477b58a27e336c55260e6ac9
task11 <task11@naver.com>
2022-05-25 12:23:11 +0900

    style: Login 페이지 레이아웃 마크업 & 스타일링

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,3 +1,5 @@
+import renderFooter from '../components/footer.js';
+import renderNav from '../components/nav.js';
 import * as Api from '/api.js';
 import { validateEmail } from '/useful-functions.js';
 
@@ -8,9 +10,11 @@ const submitButton = document.querySelector('#submitButton');
 
 addAllElements();
 addAllEvents();
+renderNav(false);
+renderFooter();
 
 // html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-async function addAllElements() {}
+async function addAllElements() { }
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
