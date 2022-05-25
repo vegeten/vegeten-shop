5b00601892594595ba8e2f980a5606a8a7a58c66
jiyoonZ <juneky416@gmail.com>
2022-05-26 00:49:30 +0900

    style:상품 목록 레이아웃

--- src/views/shop/shop.js
+++ src/views/shop/shop.js
@@ -1,7 +1,79 @@
-import * as Api from '/api.js';
+// import * as Api from '/api.js';
+import { getNode } from '../useful-functions.js';
+import renderNav from '../components/nav.js';
+import renderFooter from '../components/footer.js';
 
-async function getDataItems() {
-  const data =await Api.get('/api/shop/list');
-  
+const mockUserAPI = {
+  email: 'test@test.com',
+  fullName: '김정현',
+  password: '12341234',
+  phoneNumber: '010-5628-9304',
+  address: {
+    postalCode: '1234',
+    address1: '마포구 합정동',
+    address2: '123-123',
+  },
+  role: 'basic-user',
+};
+renderNav(mockUserAPI.role === 'basic-user' ? false : true);
+renderFooter();
+
+// 카테고리 활성화를 위해 'active' 클래스 동적추가
+const categories = document.querySelectorAll('.category');
+const categoryTitle = document.querySelector('.category-name');
+
+for (let i = 0; i < categories.length; i++) {
+  categories[i].onclick = function () {
+    if (!categories[i].classList.contains('active')) {
+      categories.forEach((ele) => ele.classList.remove('active'));
+      categories[i].classList.add('active');
+      // 클릭한 카테고리별로 제목 바꿔주기
+      categoryTitle.innerHTML = categories[i].textContent;
+    }
+  };
 }
+// 카테고리편집 모달창 활성화
+const editCategory = getNode('.editCategory');
+const modalEditCategory = getNode('#modal-editCategory');
+const editClose = getNode('#modal-editCategory button.delete');
+editCategory.onclick = () => {
+  modalEditCategory.classList.add('is-active');
+};
+editClose.onclick = () => {
+  modalEditCategory.classList.remove('is-active');
+};
 
+// 상품추가 모달창 활성화하기
+const addProduct = getNode('.addProduct');
+const modalAddProduct = getNode('.modal-addProduct');
+const addProductClose = getNode('.modal-addProduct button.delete');
+// 상품추가 버튼 클릭시 활성화
+addProduct.addEventListener('click', () => {
+  modalAddProduct.classList.add('is-active');
+});
+
+// 닫기버튼 클릭시 비활성화
+addProductClose.addEventListener('click', () => {
+  modalAddProduct.classList.remove('is-active');
+});
+
+// 파일업로드
+const fileInput = document.querySelector('#file-main input[type=file]');
+const subFileInput = document.querySelector('#file-sub input[type=file]');
+fileInput.onchange = () => {
+  if (fileInput.files.length > 0) {
+    const fileName = document.querySelector('#file-main .file-name');
+    fileName.textContent = fileInput.files[0].name;
+  }
+};
+subFileInput.onchange = () => {
+  if (subFileInput.files.length > 0) {
+    const fileName = document.querySelector('#file-sub .file-name');
+    fileName.textContent = subFileInput.files[0].name;
+  }
+};
+
+// async function getDataItems() {
+//   const data =await Api.get('/api/shop/list');
+
+// }
