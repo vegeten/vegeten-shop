b53475a42c93bc0bbd0c01c52bc6dfb8d6c7898d
task11 <task11@naver.com>
2022-05-25 23:34:31 +0900

    feat: Home 메인 이미지 캐러셀 슬라이더 구현

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,11 +1,47 @@
 import renderFooter from "../components/footer.js";
 import { renderNav } from "../components/nav.js";
+import { getNode } from "../useful-functions.js";
 
 window.onpageshow = function (event) {
   if (event.persisted) {
     window.location.reload();
   }
 };
-
 renderNav();
 renderFooter();
+
+const $carouselSlide = getNode('.carousel-slide');
+const $prevBtn = getNode('#prevBtn');
+const $nextBtn = getNode('#nextBtn');
+const slideLength = 3;
+let counter = 0;
+let size = $carouselSlide.clientWidth;
+
+const setSlideSize = (e) => {
+  console.log(e);
+  size = $carouselSlide.clientWidth;
+};
+
+const nextSlideEvent = () => {
+  if (counter >= slideLength) {
+    counter = 0;
+  } else {
+    counter++;
+  };
+  $carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  $carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+};
+
+const prevSlideEvent = () => {
+  if (counter <= 0) {
+    counter = slideLength;
+  } else {
+    counter--;
+  };
+  $carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  $carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+};
+
+$nextBtn.addEventListener('click', nextSlideEvent);
+$prevBtn.addEventListener('click', prevSlideEvent);
+window.addEventListener("resize", setSlideSize);
