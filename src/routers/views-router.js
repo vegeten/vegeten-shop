e2303fb8168cebf4bdfbf84486cf7b4117a7a3af
task11 <task11@naver.com>
2022-05-25 20:23:10 +0900

    style: About 페이지 레이아웃 마크업 & 스타일링

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -10,6 +10,7 @@ viewsRouter.use('/', serveStatic('home'));
 viewsRouter.use('/register', serveStatic('register'));
 viewsRouter.use('/login', serveStatic('login'));
 viewsRouter.use('/mypage', serveStatic('mypage'));
+viewsRouter.use('/about', serveStatic('about'));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
