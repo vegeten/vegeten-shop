4d93696fa2f31f278d34da100b8395c2226511dc
task11 <task11@naver.com>
2022-05-26 12:55:24 +0900

    style: Admin page 레이아웃 마크업&스타일링

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -11,6 +11,7 @@ viewsRouter.use('/register', serveStatic('register'));
 viewsRouter.use('/login', serveStatic('login'));
 viewsRouter.use('/mypage', serveStatic('mypage'));
 viewsRouter.use('/about', serveStatic('about'));
+viewsRouter.use('/admin', serveStatic('admin'));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
