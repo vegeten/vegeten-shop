fbe31624db08d781c0db608cea1dc258a925eb34
[SW2/서울/2반] 김태훈 <kth5954@gmail.com>
2022-05-26 11:27:03 +0900

    feat: category api 구현

--- src/routers/category-router.js
+++ src/routers/category-router.js
@@ -6,6 +6,20 @@ import { categoryService } from '../services';
 const categoryRouter = Router();
 
 // 카테고리 추가 api (아래는 /categories지만, 실제로는 /api/categories 로 요청해야 함.)
+categoryRouter.get('/categories', async (req, res, next) => {
+  try {
+    const categories = await categoryService.getCategories();
+    console.log(categories);
+    res.status(200).json({
+      statusCode: 200,
+      message: '전체 카테고리 목록 조회 성공',
+      data: categories,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
 categoryRouter.post('/categories', async (req, res, next) => {
   try {
     // Content-Type: application/json 설정을 안 한 경우, 에러를 만들도록 함.
@@ -33,6 +47,62 @@ categoryRouter.post('/categories', async (req, res, next) => {
       status: 500,
       message: error.message || 'Some error occured while creating th Category.',
     });
+  }
+});
+
+categoryRouter.patch('/categories/:categoryId', async function (req, res, next) {
+  try {
+    // content-type 을 application/json 로 프론트에서
+    // 설정 안 하고 요청하면, body가 비어 있게 됨.
+    if (is.emptyObject(req.body)) {
+      throw new Error('headers의 Content-Type을 application/json으로 설정해주세요');
+    }
+
+    // params로부터 id를 가져옴
+    const categoryId = req.params.categoryId;
+
+    // body data 로부터 업데이트할 카테고리 정보를 추출함.
+    const category = req.body.category;
+
+    // 위 데이터가 undefined가 아니라면, 즉, 프론트에서 업데이트를 위해
+    // 보내주었다면, 업데이트용 객체에 삽입함.
+    const toUpdate = {
+      ...(category && { category }),
+    };
+
+    // 카테고리 정보를 업데이트함.
+    const updatedCategoryInfo = await categoryService.setCategory(categoryId, toUpdate);
+
+    // 업데이트 이후의 카테고리 데이터를 프론트에 보내 줌
+    res.status(200).json({
+      statusCode: 200,
+      message: '카테고리 정보 수정 성공',
+      data: updatedCategoryInfo,
+    });
+  } catch (error) {
+    res.status(500).send({
+      status: 500,
+      message: error.message || 'Some error occurred while retrieving category.',
+    });
+    next(error);
+  }
+});
+
+categoryRouter.delete('/categories/:categoryId', async function (req, res, next) {
+  try {
+    const categoryId = req.params.categoryId;
+    // id에 맞는 카테고리를 삭제함
+    const deleteCategory = await categoryService.deleteCategory(categoryId);
+
+    res.status(200).json({
+      statusCode: 200,
+      message: '카테고리 삭제 성공',
+      data: {
+        deleteCategory,
+      },
+    });
+  } catch (error) {
+    next(error);
   }
 });
 
--- src/services/category-service.js
+++ src/services/category-service.js
@@ -19,6 +19,46 @@ class CategoryService {
 
     return createdNewCategory;
   }
+
+  async getCategories() {
+    const categories = await this.categoryModel.findAll();
+
+    // db에 등록된 상품이 하나도 없을 때, 에러 메시지 반환
+    if (!categories) {
+      const e = new Error('등록된 카테고리 내역이 없습니다. 새 상품을 등록해주세요.');
+      e.status = 404;
+      throw e;
+    }
+
+    return categories;
+  }
+
+  // 카테고리 수정
+  async setCategory(categoryId, toUpdate) {
+    let category = await this.categoryModel.findById(categoryId);
+    if (!category) {
+      const e = new Error('해당 카테고리의 id가 없습니다. 다시 한 번 확인해 주세요.');
+      e.status = 404;
+      throw e;
+    }
+    category = await this.categoryModel.update({
+      categoryId,
+      update: toUpdate,
+    });
+
+    return category;
+  }
+  // 카테고리 삭제
+  async deleteCategory(categoryId) {
+    let category = await categoryModel.delete(categoryId);
+    if (!category) {
+      const e = new Error('해당 카테고리의 id가 없습니다. 다시 한 번 확인해 주세요.');
+      e.status = 404;
+      throw e;
+    }
+
+    return category;
+  }
 }
 
 const categoryService = new CategoryService(categoryModel);
