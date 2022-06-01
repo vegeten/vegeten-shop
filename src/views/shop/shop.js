3b254970bce9e62fa8491b2cfee8144b24416304
task11 <task11@naver.com>
2022-06-01 15:18:01 +0900

    feat: Shop 페이지 상품 검색기능 구현

--- src/views/shop/shop.js
+++ src/views/shop/shop.js
@@ -3,6 +3,35 @@ import { getNode } from '../useful-functions.js';
 import { renderNav } from '../components/nav.js';
 import renderFooter from '../components/footer.js';
 
+const searchInput = getNode('.search-input');
+const searchButton = getNode('.search-button');
+const categoryTitle = document.querySelector('.category-name');
+
+function searchProducts(e) {
+  e.preventDefault();
+  const value = searchInput.value;
+
+  if (!value) return;
+
+  getSearchResult(value);
+};
+
+function productBySearchResult(target, count) {
+  if (count) categoryTitle.innerHTML = `'${target}'에 대한 ${count}건의 검색 결과`;
+  else categoryTitle.innerHTML = `'${target}'에 대한 검색 결과가 없습니다.`;
+}
+
+async function getSearchResult(target) {
+  try {
+    const datas = await fetch(`/api/search?keyword=${target}`);
+    const data = await datas.json();
+    showProducts(data.data, '검색', target);
+    productBySearchResult(target, data.data.productCount);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
 // 카테고리 렌더링 - Api.get 통신
 async function getCategoriesFromApi() {
   const data = await Api.get('/api/categories');
@@ -20,7 +49,6 @@ getCategoriesFromApi();
 // 카테고리별로 제목 바꾸기
 async function productByCategory() {
   // 렌더링된 상품목록으로 title 변경하기 
-  const categoryTitle = document.querySelector('.category-name');
   const categories = document.querySelectorAll('.category');
   for (let i = 0; i < categories.length; i++) {
     categories[i].onclick = async function (e) {
@@ -38,11 +66,12 @@ async function productByCategory() {
       // 카테고리별 상품조회 api
       const datas = await Api.get('/api/categories/products', e.target.id);
       showProducts(datas.data, e.target.id);
+      searchInput.value = '';
     };
   }
 }
-// 전체 상품조회하기 => 인자 api 통신을 받은 데이터, 카테고리명 
-function showProducts(data, categoryId = '') {
+// 전체 상품조회하기 => 인자 api 통신을 받은 데이터, 카테고리명, 검색 키워드
+function showProducts(data, categoryId = '', keyword = '') {
   const productList = getNode('#product-list');
   productList.innerHTML = "";
   data.products.map((product) => {
@@ -66,23 +95,32 @@ function showProducts(data, categoryId = '') {
   for (let i = 0; i < data.totalPage; i++) {
     // 전체보기가 아닐떄 
     // 카테고리 id 값으로 
-    if (categoryId !== '') {
+    if (categoryId === '검색') {
+      pagenationLink[i].addEventListener("click", () => {
+        getProductSearch(i + 1, keyword);
+      });
+    } else if (categoryId !== '') {
       pagenationLink[i].addEventListener("click", () => {
         getProductCategory(i + 1, categoryId);
       });
     } else {
       pagenationLink[i].addEventListener("click", () => {
         getProductAll(i + 1);
-        // alert("test!")
       });
     }
   }
 }
+
+async function getProductSearch(page, keyword) {
+  const datas = await fetch(`/api/search?keyword=${keyword}&page=${page}`);
+  const data = await datas.json();
+  showProducts(data.data, '검색', keyword);
+}
+
 // 카테고리별 상품목록 + 페이네이션 하기- Api.get 통신
 async function getProductCategory(page, categoryName) {
   const datas = await fetch(`/api/categories/products/${categoryName}?page=${page}`);
   const data = await datas.json();
-  console.log('카테고리별 상품목록', datas);
   showProducts(data.data, categoryName);
 }
 // 전체보기 상품목록 + 페이지네이션 - Api.get 통신
@@ -92,6 +130,11 @@ async function getProductAll(page) {
   showProducts(data.data);
 }
 
+function addAllEvents() {
+  searchButton.addEventListener('click', searchProducts);
+}
+
 renderNav();
 renderFooter();
 getProductAll(1);
+addAllEvents();
